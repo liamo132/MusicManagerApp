df734d53a98ca60121d9cf164529ba87b65f58c9
liamo132 <liamf.connell@gmail.com>
2024-03-13 14:17:14 +0000

    first commit

---
+++ src/musicmanagerapp/MM.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package musicmanagerapp;
+
+/**
+ *
+ * @author liamf
+ */
+public class MM {
+    
+    //declare vars
+    private String songName;
+}
